f3d97ea4c7add2bb477aae486d04f0ffb0fdb121
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-05-14 12:09:09 +0600

    menu card added

--- src/pages/Dashboard/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome.tsx
@@ -1,5 +1,42 @@
+import MenuCard from "@/components/shared/MenuCard";
+import PrimaryButton from "@/components/shared/PrimaryButton";
+
 const DashboardHome = () => {
-  return <div>Dashboard Home</div>;
+  return <div>
+    <div className="p-5">
+      <PrimaryButton>hello</PrimaryButton>
+    </div>
+    <div className="p-5">
+      <MenuCard
+      title="Pizza"
+      size="Large"
+      availability="Available"
+      price="$10.99"
+      description="Delicious cheese pizza with fresh ingredients."
+      imageUrl="https://example.com/pizza.jpg"  // Replace with your image URL
+    />
+    </div>
+    <div className="p-5">
+      <MenuCard
+      title="Pizza"
+      size="Large"
+      availability="Available"
+      price="$10.99"
+      description="Delicious cheese pizza with fresh ingredients."
+      imageUrl="https://example.com/pizza.jpg"  // Replace with your image URL
+    />
+    </div>
+    <div className="p-5">
+      <MenuCard
+      title="Pizza"
+      size="Large"
+      availability="Available"
+      price="$10.99"
+      description="Delicious cheese pizza with fresh ingredients."
+      imageUrl="https://example.com/pizza.jpg"  // Replace with your image URL
+    />
+    </div>
+  </div>;
 };
 
 export default DashboardHome;
