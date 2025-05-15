a236a9584a41ccc428dd60bbc0caad098221384e
enamulhimel <mhasan173013@bscse.uiu.ac.bd>
2025-05-15 09:23:21 +0600

    add

--- src/pages/DynamciRestaurant.tsx
+++ src/pages/DynamciRestaurant.tsx
@@ -1,9 +1,15 @@
+import { Navbar } from "@/components/dynamicResturants/Navbar";
 import { useParams } from "react-router-dom";
 
 const DynamicRestaurant = () => {
   const { id } = useParams();
   console.log(id);
-  return <div>DynamicRestaurant</div>;
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Navbar />
+    </div>
+  );
 };
 
 export default DynamicRestaurant;
