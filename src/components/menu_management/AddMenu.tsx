082ce8120c2845cb3978a29a6d4fd6b85d64a476
kazimehedihasan1 <mehedihasanarabi00@gmail.com>
2025-05-19 11:36:39 +0600

    add all menu table

---
+++ src/components/menu_management/AddMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddMenu = () => {
+    return (
+        <div>
+            AddMenu
+        </div>
+    );
+};
+
+export default AddMenu;
---
+++ src/components/menu_management/Categories.tsx
@@ -0,0 +1,12 @@
+
+
+const Categories = () => {
+    return (
+        <div>
+            Categories
+            Categories
+        </div>
+    );
+};
+
+export default Categories;
